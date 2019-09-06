bd06d2ed0e896149ef2be96043bb7122fd228d5f
danivenk <danivenk@gmail.com>
2019-09-06 22:07:25 +0000

    input test complete and functional

--- Module-1_C/input-test/input.c
+++ Module-1_C/input-test/input.c
@@ -1,30 +1,65 @@
 #include <stdio.h>
 
+int power(int x, int y);
+int get_int(char str[124]);
+
 int main(void)
 {
+    int minutes = get_int("How many minutes do you shower? (in whole minutes) ");
+
+    printf("%d\n", minutes);
+}
+
+int get_number(char str[124])
+{
+    int output = 0;
+
     while (1)
     {
         char input[24];
         char numbers[10] = "0123456789";
+        int power_10 = 0;
 
-        printf("integer please: ");
+        printf("%s", str);
         scanf("%s", &input[0]);
 
-        for (int i = 0; i < 24; i++)
+        for (int i = 23; i >= 0; i--)
         {
             for (int j = 0; j < 10; j++)
             {
                 if (input[i] == numbers[j])
                 {
-                    printf("%d\n", j);
+                    output += j*power(10, power_10);
+                    power_10++;
+                } else
+                {
+                    return 0;
                 }
             }
         }
-
-        printf("%s\n", input);
-
-        return 0;
     }
 
-    return 0;
+    return output;
+}
+
+int power(int x, int y)
+{
+    int result = x;
+
+    if (y == 0)
+    {
+        result = 1;
+    } else if (y == 1)
+    {
+        return result;
+    } else
+    {
+        while (y > 1)
+        {
+            result *= x;
+            y--;
+        }
+    }
+
+    return result;
 }
