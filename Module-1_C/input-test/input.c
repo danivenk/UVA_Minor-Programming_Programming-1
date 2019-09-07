05e3d64b5e3bd0515eb3feb862e5b6412b663c83
danivenk <danivenk@gmail.com>
2019-09-07 13:58:36 +0000

    Fixed input

--- Module-1_C/input-test/input.c
+++ Module-1_C/input-test/input.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int power(int x, int y);
-int get_int(char str[124]);
+int get_number(char str[124]);
 
 int main(void)
 {
-    int minutes = get_int("How many minutes do you shower? (in whole minutes) ");
+    int minutes = get_number("How many minutes do you shower? (in whole minutes) ");
 
     printf("%d\n", minutes);
 }
@@ -19,23 +19,28 @@ int get_number(char str[124])
         char input[24];
         char numbers[10] = "0123456789";
         int power_10 = 0;
+        int found = 0;
 
         printf("%s", str);
         scanf("%s", &input[0]);
 
         for (int i = 23; i >= 0; i--)
         {
+
             for (int j = 0; j < 10; j++)
             {
                 if (input[i] == numbers[j])
                 {
                     output += j*power(10, power_10);
                     power_10++;
-                } else
-                {
-                    return 0;
+                    found++;
                 }
             }
+        }
+
+        if (found != 0)
+        {
+            break;
         }
     }
 
