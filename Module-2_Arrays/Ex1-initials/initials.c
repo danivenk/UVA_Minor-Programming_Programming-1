ae540a7a70e1ed66c59ffcb4bcd108aae7e4a2f9
danivenk <danivenk@gmail.com>
2019-09-13 10:20:03 +0000

    Final version (submitted version)

--- Module-2_Arrays/Ex1-initials/initials.c
+++ Module-2_Arrays/Ex1-initials/initials.c
@@ -1,4 +1,4 @@
-
+// initials.c give the initials of any name the user prompts
 // Dani van Enk, 11823526
 
 // including the standard I/O, standard library and ctype library
@@ -6,31 +6,40 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+// main loop
 int main(void)
 {
     // defining SIZE and predefining the user_input
-    int size = 128;
+    int size = 1028;
     char input[size];
 
+    // promting user_input
     printf("Please put your name: ");
     fgets(input, size, stdin);
 
+    // loop for the initials
     for (int i = 0; i < size; i++)
     {
-
+        // print the character after an space character
         if (input[i] == ' ' && input[i + 1] != ' ' && input[i] != '\n')
         {
             printf("%c", toupper(input[i + 1]));
         }
+        // print the fist character
         else if (i == 0 && input[i] != ' ')
         {
             printf("%c", toupper(input[i]));
         }
+        // break at end of string
         else if (input[i] == '\0')
         {
             break;
         }
     }
 
+    // line break
     printf("\n");
+
+    // end program
+    return 0;
 }
