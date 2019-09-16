a3e9042a02c4fae0ef3b022f99722771c573b2e3
danivenk <danivenk@gmail.com>
2019-09-16 16:44:44 +0000

    Alphabetical counter works up to AzzA (len=3 works)

--- Module-2_Arrays/Ex3-crack/crack.c
+++ Module-2_Arrays/Ex3-crack/crack.c
@@ -8,47 +8,87 @@
 #include <unistd.h>
 #include <crypt.h>
 
+char next_letter(char current_letter);
+int no_of_z(char string[], int length);
+
 int main(int argc, char *argv[])
 {
     if (argc == 2)
     {
         // defining SIZE and predefining the user_input
-        int size = 2;
+        int size = 6;
+        int least_bit_pos = 0;
+        int length = 1;
+        int roll_over = 0;
+        int next_digit = 0;
         char *hash = argv[1];
         char password[size];
+        char lastpass[size];
         char salt[3] = {hash[0], hash[1]};
+        char input = 0;
 
         for (int i = 0; i < size; i++)
         {
             password[i] = '\0';
+            lastpass[i] = '\0';
         }
 
         while (1)
         {
-            for (int i = 0; i <= size; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < i; j++)
+                lastpass[i] = password[i];
+            }
+
+            if (lastpass[0] == 'z' && lastpass[least_bit_pos + 1] == '\0' &&
+                no_of_z(lastpass, length) == length)
+            {
+                least_bit_pos++;
+                length++;
+                for (int i = least_bit_pos - 1; i >= 0; i--)
                 {
-                    for (int k = 65; k <= 122; k++)
+                    password[i] = next_letter(lastpass[i]);
+                }
+            }
+            password[least_bit_pos] = next_letter(lastpass[least_bit_pos]);
+
+            // printf("Try: %s, Length: %d\n", password, length);
+
+            for (int i = 1; i <= length; i++)
+            {
+                if (lastpass[i] == 'z' && (lastpass[i + 1] == 'z' || lastpass[i + 1] == '\0'))
+                {
+                    password[i - 1] = next_letter(lastpass[i - 1]);
+                    if (length > 3 && no_of_z(lastpass, size) >= 2)
                     {
-                        if (k == 90)
-                        {
-                            k += 7;
-                        }
-                        password[j] = (char) k;
-
-                        printf("Try: %s, i %d, j %d, k %d\n", password, i, j, k);
-
-                        if (hash == crypt(password, salt))
-                        {
-                            printf("Found: %s\n", password);
-                            return 0;
-                        }
+                        fgets(&input, size, stdin);
+                        printf("Try: %s\n", password);
                     }
                 }
             }
 
-            return 0;
+            // printf("Try: %s, Length: %d\n", password, length);
+
+
+            // printf("Try: %s\n", password);
+
+
+            // if (password[0] == 'z' && password[1] == 'z')
+            // {
+            //     fgets(&input, size, stdin);
+            // }
+            if (hash == crypt(password, salt))
+            {
+                printf("Found: %s\n", password);
+                return 0;
+            }
+            else if (length > 5)
+            {
+                printf("Not Found 404\n");
+                return 1;
+            }
+
+            //fgets(&input, size, stdin);
         }
     }
     // error message for not enough parameters
@@ -60,4 +100,37 @@ int main(int argc, char *argv[])
 
     // end program
     return 0;
+}
+
+char next_letter(char current_letter)
+{
+    int i = (int) current_letter;
+    if (i == 90)
+    {
+        i += 7;
+    }
+    else if (i == 122 || i == 0)
+    {
+        i = 65;
+    }
+    else
+    {
+        i++;
+    }
+
+    return (char) i;
+}
+
+int no_of_z(char string[], int length)
+{
+    int no_z = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (string[i] == 'z')
+        {
+            no_z++;
+        }
+    }
+
+    return no_z;
 }
